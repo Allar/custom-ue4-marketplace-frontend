e3206c5207a0351de3ac8ca2e2046300cb977fe0
Michael Allar <allar@michaelallar.com>
2015-12-06 00:03:00 -0800

    Completed auth chain

--- js/api.js
+++ js/api.js
@@ -1,60 +1,138 @@
 global.request = (global.request === undefined) ? require('request') : global.request;
+global.request = request.defaults({followRedirect: false, followAllRedirects: false});
 
 // Get the current window
 global.win = global.window.nwDispatcher.requireNwGui().Window.get();
 
 global.fakeJar = {};
 
+require('events').EventEmitter.prototype._maxListeners = 100;
+
 var api = function () {};
 
-api.prototype.getSSOSession = function () {
+api.prototype.updateFakeJar = function(set_cookie_array) {
+	for (var i = 0; i < set_cookie_array.length; ++i) {
+		var cookie_pair = set_cookie_array[i].split(';',1)[0].split('=');
+		global.fakeJar[cookie_pair[0]] = cookie_pair[1];
+		if(cookie_pair[1] == 'invalid') {
+			delete global.fakeJar[cookie_pair[0]];
+		}
+	}
+}
+
+api.prototype.getFakeJarCookieString = function () {
+	var cookieString = "";
+	for(var key in global.fakeJar) {
+    	cookieString += key + '=' + global.fakeJar[key] + '; ';
+	}
+	console.log(cookieString);
+	return cookieString;
+}
+
+api.prototype.rebuildLogin = function (body) {
+	$('body').html($(body).find('form'));
+	$('#rememberMeCheckContainer').remove();
+	$('.resetPass').remove();
 	
+	$('#epic_username').removeAttr('oninput');
+	$('#password').removeAttr('oninput');
+	$('#password').removeAttr('oninput');
+	$('#signIn').attr('onclick','api.doLogin()');
+	
+}
+
+api.prototype.getLogin = function () {
 	var opts = {
 		uri: 'https://accounts.unrealengine.com/login/doLogin',
 	};
 	
-	//global.jar.setCookie(request.cookie("EPIC_SSO_SESSION=1azvl2lqqgvug1sh96q5mkqokj;Path=/;Domain=.unrealengine.com;Secure;HttpOnly", ".unrealengine.com"));
+	request.get(opts, function (error, response, body) {
+		module.exports.updateFakeJar(response.headers['set-cookie']);		
+		module.exports.rebuildLogin(body);
+		console.log(fakeJar);
+	});
+}
+
+api.prototype.getTracked = function () {
+	var opts = {
+		uri: 'https://tracking.unrealengine.com/track.png',
+		qs: { client_id: '43e2dea89b054198a703f6199bee6d5b' }		
+	};
 	
 	request.get(opts, function (error, response, body) {
-		//if (response.headers['set-cookie'])
-		for (var i = 0; i < response.headers['set-cookie'].length; ++i) {
-			var cookie_pair = response.headers['set-cookie'][i].split(';',1)[0].split('=');
-			fakeJar[cookie_pair[0]] = cookie_pair[1];
-		}
-		
-		
-		
+		module.exports.updateFakeJar(response.headers['set-cookie']);		
 		console.log(fakeJar);
-		
 	});
 }
 
 api.prototype.doLogin = function() {
-	var username = window.document.getElementById('epic_username').value;
-	var password = window.document.getElementById('password').value;
-	
-	var cookieString = "";
-	for(var key in fakeJar) {
-    	cookieString += key + '=' + fakeJar[key] + '; ';
-	}
-	console.log(cookieString);
-	
 	var opts = {
 		uri: 'https://accounts.unrealengine.com/login/doLogin',
-		form:{fromForm:'yes',epic_username:username, password:password},
-		headers: { Cookie:cookieString }
+		form:$('form#loginForm').serializeObject(),
+		headers: { Cookie:module.exports.getFakeJarCookieString(), 'Origin': 'allar_alternative_marketplace' },
+		qs: { client_id: '43e2dea89b054198a703f6199bee6d5b' }
 	};
 	
-	request.post(opts , function(error, response, body){
-		console.log(response.headers);
-		if (response.statusCode == 400)
+	request.post(opts, function(error, response, body) {
+		module.exports.updateFakeJar(response.headers['set-cookie']);
+	
+		if (response.statusCode == 400) // login failure
 		{
-			console.log('ohshit');
+			module.exports.rebuildLogin(body);
+		} else if (response.statusCode == 302) // success
+		{
+			$('body').html('Authorizing...');
+			module.exports.authorize();
 		}
-		if (response.headers['access-control-expose-headers'] === 'X-EPIC-LOGIN-COMPLETE-REDIRECT') {
-			console.log('holy shit');
+		else {
+			console.log(response.statusCode);
 		}
-		console.log(response.statusCode);
+		
+	});
+}
+
+// Kick off what appears to be an OAuth attempt but this seems to get all the SSO information needed without
+// exchanging the resulting OAuth token. Perhaps oauth flow is meant for non-web clients i.e. games
+// OAuth generally requires a 'state' field as well but its not needed here?
+api.prototype.authorize = function () {	
+	var opts = {
+		uri: 'https://accounts.unrealengine.com/authorize/index',
+		headers: { Cookie:module.exports.getFakeJarCookieString(), 'Origin': 'allar_alternative_marketplace' },
+		qs: { client_id: '43e2dea89b054198a703f6199bee6d5b', response_type: 'code', forWidget: 'true' }
+	};
+	
+	request.get(opts, function(error, response, body) {
+		module.exports.updateFakeJar(response.headers['set-cookie']);
+		
+		if (response.statusCode == 200 && response.headers['access-control-expose-headers'] == 'X-EPIC-LOGIN-COMPLETE-REDIRECT') {
+			var json = JSON.parse(body);
+			var code = json.redirectURL.split('?code=')[1];
+			$('body').html('Successfully Authorized! Performing Exchange...');
+			module.exports.exchange(code);
+		} else {
+			$('body').html(JSON.stringify(response, null, ' '));
+		}
+	});
+}
+
+// I don't really know what this does, its supposed to exchange our OAuth code for a token but
+// Epic's web login process doesn't require one. Maybe their web side has an incomplete OAuth process
+// or simply doesn't use it? SSO information got from 'authorize' appears to be enough. Scary?
+api.prototype.exchange = function (code) {
+	var opts = {
+		uri: 'https://www.unrealengine.com/exchange',
+		headers: { Cookie:module.exports.getFakeJarCookieString(), 'connection': 'keep-alive', 'Upgrade-Insecure-Requests': '1', 'Referer': 'https://accounts.unrealengine.com/login/index?client_id=43e2dea89b054198a703f6199bee6d5b' },
+		qs: { code: code }
+	};
+	
+	request.get(opts, function(error, response, body) {
+		module.exports.updateFakeJar(response.headers['set-cookie']);
+		
+		if (response.statusCode == 302) {
+			$('body').html('Successfully Completed Auth Chain!');
+		} else {
+			$('body').html(JSON.stringify(response, null, ' '));
+		}
 	});
 }
 
